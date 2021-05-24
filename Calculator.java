1269de13ac6a579832eb062e094f81d0dd547262
valerka96 <72154364+valerka96@users.noreply.github.com>
2021-05-24 15:25:11 +0200

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -21,8 +21,7 @@ public class Factorial {
 	
 	
 	public static void main(String[] args) {
-		int value = Integer.parseInt(args[0]);
-		Factorial value1 = new Factorial(value);
+		Factorial value1 = new Factorial(Integer.parseInt(args[0]));
 		value1.findFactorial();
 		}
 }
